1932c57b01a980c22403448b2c1ffe48f35e9eaa
panosy <panosy@gmail.com>
2021-06-03 15:08:58 +0300

    JSON Parser for Loudly, Panagiotis Giotis

--- DerivedData/JSON Parser/Build/Intermediates.noindex/JSON Parser.build/Debug/JSON Parser.build/Objects-normal/x86_64/JSONParser.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/pi/Desktop/JSON/JSON\ Parser/JSON\ Parser/JSONParser.cpp \
-  /Users/pi/Desktop/JSON/JSON\ Parser/JSON\ Parser/JSONParser.h
--- DerivedData/JSON Parser/Build/Intermediates.noindex/JSON Parser.build/Debug/JSON Parser.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/pi/Desktop/JSON/JSON\ Parser/JSON\ Parser/main.cpp \
-  /Users/pi/Desktop/JSON/JSON\ Parser/JSON\ Parser/JSONParser.h
